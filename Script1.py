4193aa29ca0300a953563019d10cd4fc08ee336d
higgi22k <higgi22k@gmail.com>
2019-10-09 20:05:23 -0400

    katies first repo

---
+++ Script1.py
@@ -0,0 +1,10 @@
+person_a = 100
+person_b = 500
+
+
+if person_a > person_b:
+    print("Person b is an idiot!")
+elif person_b > person_a:
+    print("Person b has their shit together")
+else:
+    print("Nah, person b is a baller")
